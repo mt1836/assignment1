e83dccaf68d7550dfd7b84b7101456d69063bf6a
Michael Tse <mt1836@nyu.edu>
2019-09-12 22:44:04 -0400

    Adding Week 2 Assignment Files

---
+++ spell.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "dictionary.h"
+
+int main()
+{
+    printf("hello world");
+    return 0;
+}
