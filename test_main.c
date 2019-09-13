12420d7ac67a06a9d1c48b0d28067982be8f6214
Michael Tse <mt1836@nyu.edu>
2019-09-13 18:56:08 -0400

    Updating all files

---
+++ test_main.c
@@ -0,0 +1,105 @@
+#include <check.h>
+#include "dictionary.h"
+#include <stdlib.h>
+
+#define DICTIONARY "wordlist.txt"
+<<<<<<< HEAD
+#define TESTDICT "test_worlist.txt"
+
+START_TEST(test_dictionary_normal)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    ck_assert(load_dictionary(TESTDICT, hashtable));
+    // Here we can test if certain words ended up in certain buckets
+    // to ensure that our load_dictionary works as intended. I leave
+    // this as an exercise.
+}
+END_TEST
+=======
+>>>>>>> e83dccaf68d7550dfd7b84b7101456d69063bf6a
+
+START_TEST(test_check_word_normal)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    const char* correct_word = "Justice";
+<<<<<<< HEAD
+    const char* punctuation_word_2 = "pl.ace";
+    ck_assert(check_word(correct_word, hashtable));
+    ck_assert(!check_word(punctuation_word_2, hashtable));
+    // Test here: What if a word begins and ends with "?
+}
+END_TEST
+
+START_TEST(test_check_words_normal)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    char* expected[3];
+    expected[0] = "sogn";
+    expected[1] = "skyn";
+    expected[2] = "betta";
+    char *misspelled[MAX_MISSPELLED];
+    FILE *fp = fopen("test1.txt", "r");
+    int num_misspelled = check_words(fp, hashtable, misspelled);
+    ck_assert(num_misspelled == 3);
+    bool test = strlen(misspelled[0]) == strlen(expected[0]);
+    int len1 = strlen(misspelled[0]);
+    int len2 = strlen(expected[0]);
+    ck_assert_msg(test, "%d!=%d", len1, len2);
+    ck_assert_msg(strcmp(misspelled[0], expected[0]) == 0);
+    ck_assert_msg(strcmp(misspelled[1], expected[1]) == 0);
+    ck_assert_msg(strcmp(misspelled[2], expected[2]) == 0);
+=======
+    const char* incorrect_word = "Caoimhín";
+    ck_assert(check_word(correct_word, hashtable));
+    ck_assert(!check_word(incorrect_word, hashtable));
+}
+END_TEST
+
+START_TEST(test_check_word_buffer_overflow)
+{
+    hashmap_t hashtable[HASH_SIZE];
+    load_dictionary(DICTIONARY, hashtable);
+    char incorrect_word[500000];
+    for(int i = 0; i < 499999; i++)
+        incorrect_word[i] = 'A';
+    incorrect_word[499999] = 0;
+    ck_assert(!check_word(incorrect_word, hashtable));
+>>>>>>> e83dccaf68d7550dfd7b84b7101456d69063bf6a
+}
+END_TEST
+
+Suite *
+check_word_suite(void)
+{
+    Suite * suite;
+    TCase * check_word_case;
+    suite = suite_create("check_word");
+    check_word_case = tcase_create("Core");
+    tcase_add_test(check_word_case, test_check_word_normal);
+<<<<<<< HEAD
+    tcase_add_test(check_word_case, test_check_words_normal);
+=======
+    tcase_add_test(check_word_case, test_check_word_buffer_overflow);
+>>>>>>> e83dccaf68d7550dfd7b84b7101456d69063bf6a
+    suite_add_tcase(suite, check_word_case);
+
+    return suite;
+}
+
+int
+main(void)
+{
+    int failed;
+    Suite *suite;
+    SRunner *runner;
+    
+    suite = check_word_suite();
+    runner = srunner_create(suite);
+    srunner_run_all(runner, CK_NORMAL);
+    failed = srunner_ntests_failed(runner);
+    srunner_free(runner);
+    return (failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE);
+}
+
